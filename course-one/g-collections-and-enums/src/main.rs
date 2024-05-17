ea723d82a35227ef0416e10ae6ed686353c60002
Daniel Dimitric <daniel.dimitric@protonmail.com>
2024-05-17 13:26:20 +0200

    exercise g

--- course-one/g-collections-and-enums/src/main.rs
+++ course-one/g-collections-and-enums/src/main.rs
@@ -1,48 +1,6 @@
 // Silence some warnings that could distract from the exercise
 #![allow(unused_variables, unused_mut, dead_code)]
 
-// Someone is shooting arrows at a target.  We need to classify the shots.
-//
-// 1a. Create an enum called `Shot` with variants:
-// - `Bullseye`
-// - `Hit`, containing the distance from the center (an f64)
-// - `Miss`
-//
-// You will need to complete 1b as well before you will be able to run this program successfully.
-
-impl Shot {
-    // Here is a method for the `Shot` enum you just defined.
-    fn points(self) -> i32 {
-        // 1b. Implement this method to convert a Shot into points
-        // - return 5 points if `self` is a `Shot::Bullseye`
-        // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
-        // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
-        // - return 0 points if `self` is a Miss
-    }
-}
-
-fn main() {
-    // Simulate shooting a bunch of arrows and gathering their coordinates on the target.
-    let arrow_coords: Vec<Coord> = get_arrow_coords(5);
-    let mut shots: Vec<Shot> = Vec::new();
-
-    // 2. For each coord in arrow_coords:
-    //
-    //   A. Call `coord.print_description()`
-    //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
-    //   `coord.distance_from_center()`
-    //      - Less than 1.0 -- `Shot::Bullseye`
-    //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
-    //      - Greater than 5.0 -- `Shot::Miss`
-
-
-    let mut total = 0;
-    // 3. Finally, loop through each shot in shots and add its points to total
-
-    println!("Final point total is: {}", total);
-}
-
-// A coordinate of where an Arrow hit
 #[derive(Debug)]
 struct Coord {
     x: f64,
@@ -60,10 +18,26 @@ impl Coord {
             self.x,
             self.y);
     }
-
 }
 
-// Generate some random coordinates
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
+
+impl Shot {
+    fn points(self) -> i32 {
+        match self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) => { if x < 3.0 { 2 } else { 1 } },
+            // Shot::Hit(x) if x < 3.0 => 2,    ALTERNATIVE WAY USING GUARDS
+            // Shot::Hit(x) => 1,               ALTERNATIVE WAY USING GUARDS
+            Shot::Miss => 0,
+        }
+    }
+}
+
 fn get_arrow_coords(num: u32) -> Vec<Coord> {
     let mut coords: Vec<Coord> = Vec::new();
     for _ in 0..num {
@@ -74,4 +48,28 @@ fn get_arrow_coords(num: u32) -> Vec<Coord> {
         coords.push(coord);
     }
     coords
+}
+
+fn main() {
+    let arrow_coords: Vec<Coord> = get_arrow_coords(5);
+    let mut shots: Vec<Shot> = Vec::new();
+    let mut total = 0;
+
+    for coord in arrow_coords {
+        coord.print_description();
+
+        let shot = match coord.distance_from_center() {
+            x if x < 1.0 => { Shot::Bullseye },
+            x if x < 5.0 => { Shot::Hit(x) },
+            _ => { Shot::Miss },
+        };
+
+        shots.push(shot);
+    }
+
+    for shot in shots {
+        total += shot.points();
+    }
+
+    println!("Final point total is: {}", total);
 }
